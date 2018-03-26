432fab8e19dc4170e9d9388e18e1c1cacaa4d723
kadroff <marsel.kadroff@gmail.com>
2018-03-27 01:05:12 +0500

    удаляет выбранный li

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 var noteList = [];
 
 add_note.onclick = (function(){
-  var title = document.createElement("INPUT");
-  title.setAttribute("type", "text");
-  title.className = "input_title";
-  right_column.appendChild(title);
+    var title = document.createElement("INPUT");
+    title.setAttribute("type", "text");
+    title.className = "input_title";
+    right_column.appendChild(title);
 
-  var name = document.createElement("INPUT");
-  name.setAttribute("type", "text");
-  name.className = "input_name";
-  right_column.appendChild(name);
+    var name = document.createElement("INPUT");
+    name.setAttribute("type", "text");
+    name.className = "input_name";
+    right_column.appendChild(name);
 
-  create_note.onclick = (function(){
-    var list = {};
-    list.note = title.value;
-    list.check = false;
-    var noteList_len = noteList.length;
-    noteList[noteList_len] = list;
+    create_note.onclick = (function(){
+      var list = {};
+      list.note = title.value;
+      list.check = false;
+      var noteList_len = noteList.length;
+      noteList[noteList_len] = list;
 
-    let create_element = document.createElement("li");
-  //  create_element.innerHTML = '<input type="radio">' + '<h3>' + title.value + '</h3>' + '<p>' + name.value + '</p>';
-  //  note.appendChild(create_element); 
+      let create_element = document.createElement("li");
+    //  create_element.innerHTML = '<input type="radio">' + '<h3>' + title.value + '</h3>' + '<p>' + name.value + '</p>';
+    //  note.appendChild(create_element); 
 
-    checked();
+      checked();
 
-  function checked() {
-    for (key in noteList) {
-      if(list.check == true) {
-        create_element.innerHTML =  "<input id='checkbox' type='checkbox'> checked" +  title.value + '</h3>' + '<p>' + name.value + '</p>';
-      }else create_element.innerHTML = "<input id='checkbox' type='checkbox'>" +  title.value + '</h3>' + '<p>' + name.value + '</p>';  
-      note.appendChild(create_element); 
+    function checked() {
+      for (key in noteList) {
+        if(list.check == true) {
+          create_element.innerHTML = "<input id='checkbox' type='checkbox'> checked" +  title.value + '</h3>' + '<p>' + name.value + '</p>';
+        }else create_element.innerHTML = "<input id='checkbox' type='checkbox'>" +  title.value + '</h3>' + '<p>' + name.value + '</p>';  
+        note.appendChild(create_element); 
+      }
     }
-  }s
+
+    document.getElementById('checkbox').onclick = function() {
+      if (this.checked) {
+        this.remove();
+      }
+    }; 
   });
 });
