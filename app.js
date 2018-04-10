2184f31e0fe2790edd8ba25f752f5ba34084d78c
kadroff <marsel.kadroff@gmail.com>
2018-04-11 00:02:12 +0500

    рефакторинг с помощью localStorage

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
-var noteList = [];
 
-add_note.onclick = (function(){
-    var title = document.createElement("INPUT");
-    title.setAttribute("type", "text");
-    title.className = "input_title";
-    right_column.appendChild(title);
 
-    var name = document.createElement("INPUT");
-    name.setAttribute("type", "text");
-    name.className = "input_name";
-    right_column.appendChild(name);
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
+let note = '';
 
-    create_note.onclick = (function(){
-      var list = {};
-      list.note = title.value;
-      list.check = false;
-      var noteList_len = noteList.length;
-      noteList[noteList_len] = list;
+const editor = document.querySelector('#editor');
+const right_column = document.querySelector('#right_column');
+const notesList = document.querySelector('#noteList');
 
-      let create_element = document.createElement("li");
-    //  create_element.innerHTML = '<input type="radio">' + '<h3>' + title.value + '</h3>' + '<p>' + name.value + '</p>';
-    //  note.appendChild(create_element); 
+editor.onkeyup = () => {
+  note = editor.value;
+  localStorage.setItem('note', note);
+}
 
-      checked();
+function newNote() {
+  notes.push(note);
+  localStorage.setItem('notes', JSON.stringify(notes));
+  renderNotes();
 
-    function checked() {
-      for (key in noteList) {
-        if(list.check == true) {
-          create_element.innerHTML = "<input id='checkbox' type='checkbox'> checked" +  title.value + '</h3>' + '<p>' + name.value + '</p>';
-        }else create_element.innerHTML = "<input id='checkbox' type='checkbox'>" +  title.value + '</h3>' + '<p>' + name.value + '</p>';  
-        note.appendChild(create_element); 
-      }
-    }
+  note = '';
+  editor.value = note;
+}
 
-    document.getElementById('checkbox').onclick = function() {
-      if (this.checked) {
-        this.remove();
-      }
-    }; 
+function renderNotes() {
+	notesList.innerHTML = ''; 
+	notes.forEach(note => {
+  	li = document.createElement('li');
+    li.innerText = note;
+  	notesList.appendChild(li);
   });
-});
+}
+
+renderNotes();
+
+
+
+
+
